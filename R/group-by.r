3588f246269999d61e44648b00704cf3856ae2ec
Romain Francois <romain@rstudio.com>
2021-01-14 09:41:07 +0100

    defunct underscore and _each() functions which have been deprecated since 0.7.0 (2017)

--- R/group-by.r
+++ R/group-by.r
@@ -143,9 +143,7 @@ group_by_prepare <- function(.data, ..., .add = FALSE, .dots = deprecated(), add
 
   new_groups <- enquos(...)
   if (!missing(.dots)) {
-    # Used by dbplyr 1.4.2 so can't aggressively deprecate
-    lifecycle::deprecate_warn("1.0.0", "group_by(.dots = )")
-    new_groups <- c(new_groups, compat_lazy_dots(.dots, env = caller_env()))
+    lifecycle::deprecate_stop("1.0.0", "group_by(.dots = )")
   }
   new_groups <- new_groups[!map_lgl(new_groups, quo_is_missing)]
 
